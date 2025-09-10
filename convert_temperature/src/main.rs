3a5799b1ae08eadedccae0df82dfea1b36f22458
Ram Vasani <96667000+rvasan1310@users.noreply.github.com>
2025-09-10 16:46:40 -0400

    Update main.rs

--- convert_temperature/src/main.rs
+++ convert_temperature/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let celsius = 23.0;
+    let fahrenheit = celsius_to_fahrenheit(celsius);
+
+    assert_eq!(fahrenheit, 73.4);
+    println!("Test passed!");
+}
+
+fn celsius_to_fahrenheit(temp: f64) -> f64 {
+    (temp * 1.8 + 32.0)
 }
