c67552d567ba9e11e004ce00801c1f8925da1049
Ram Vasani <96667000+rvasan1310@users.noreply.github.com>
2025-09-10 16:57:29 -0400

    Update main.rs

--- max_min_mean/src/main.rs
+++ max_min_mean/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let numbers - [1,9,-2,0.23,20,-7,13,37,20,56,-18,20,3];
+    let mut max = i32;
+    let mut min = i32;
+    let mut sum = f64;
+
+    max = numbers[0];
+    min = numbers[0];
+    mean = 0.0;
+
+    for &num in numbers.iter(){
+        if num > max{
+            max = num;
+        }
+        if num < min{
+            min = num;
+        }
+    }
+
+    mean /= numbers.len() as f64;
 }
