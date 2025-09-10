c8893129d734d33a35df05fce7d99a264c1b29b2
Ram Vasani <96667000+rvasan1310@users.noreply.github.com>
2025-09-10 17:06:08 -0400

    Update main.rs

--- find_the_average/src/main.rs
+++ find_the_average/src/main.rs
@@ -1,11 +1,10 @@
-fn main() {
-    let celsius = 23.0;
-    let fahrenheit = celsius_to_fahrenheit(celsius);
+fn main (){
+    let a = 13;
+    let b = 2.3;
+    letc: f32 = 120.0;
 
-    assert_eq!(fahrenheit, 73.4);
+    let average = (a as f32 + b + c as f64) / 3.0;
+
+    assert_eq!(average, 45.1);
     println!("Test passed!");
-}
-
-fn celsius_to_fahrenheit(temp: f64) -> f64 {
-    (temp * 1.8 + 32.0)
 }
