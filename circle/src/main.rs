807132fc8afa09b5feb2c57db5ee276ba3414f32
Ram Vasani <96667000+rvasan1310@users.noreply.github.com>
2025-09-21 17:10:16 -0400

    adds circle.rs

--- circle/src/main.rs
+++ circle/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+struct Circle{
+    radius: f64,
+    pi: f64
+    
+}
+
+impl Circle{
+    fn new(radius: f64, pi : f64) -> Circle{
+        Circle {radius, pi}
+    }
+    
+    fn area(&self) -> f64{
+        self.pi * ((self.radius)*(self.radius))
+    }
+    
+    fn circumference(&self) -> f64{
+        2.0 * self.pi * self.radius
+    }
+}
+
+fn main(){
+    let circle = Circle::new(8.0, 3.14);
+    println!("Area: { } ", circle.area());
+    println!("CIrcumference: { }", circle.circumference());
 }
