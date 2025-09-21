11b3a6fc58b0c759a73af2f18e50d3d9aaee1a6f
Ram Vasani <96667000+rvasan1310@users.noreply.github.com>
2025-09-21 17:11:10 -0400

    adds rectangle.rs

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,3 +1,39 @@
+struct Rectangle {
+    width: f64,
+    height: f64,
+   
+}
+
+impl Rectangle {
+    fn new(width: f64, height: f64) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn area(&self) -> f64 {
+        // TODO: implement
+        self.width*self.height
+    }
+
+    fn perimeter(&self) -> f64 {
+        // TODO: implement
+        2.0 * (self.height + self.width)
+    }
+
+    fn is_square(&self) -> bool {
+        // TODO: implement
+        self.width == self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle::new(10.0, 5.0);
+    println!("Area: {}", rect.area());
+    println!("Perimeter: {}", rect.perimeter());
+    println!("Is square? {}", rect.is_square());
+
+    assert!(Rectangle::new(5.0, 5.0).is_square());
+    assert!(!Rectangle::new(5.0, 6.0).is_square());
+}
 fn main() {
     println!("Hello, world!");
 }
